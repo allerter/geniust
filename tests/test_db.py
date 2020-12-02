319eb7a4d455c185e7569e65598dcd113957804e
Allerter <45076212+Allerter@users.noreply.github.com>
2020-12-02 21:11:31 +0330

    - added remaining top-level tests files - added data needed for tests

---
+++ tests/test_db.py
@@ -0,0 +1,188 @@
+from unittest.mock import patch, MagicMock
+
+import pytest
+
+from geniust import db, constants
+
+
+@pytest.fixture(scope='module')
+def database():
+    return db.Database('test_table')
+
+
+@patch('psycopg2.connect')
+def test_get_cursor(connection):
+    func = MagicMock()
+    res = db.get_cursor(func)
+    res(1, 2, a=1)
+
+    connection.assert_called_once_with(constants.DATABASE_URL,
+                                       sslmode='require')
+
+
+def test_user_in_db(database):
+    user_data = {}
+    chat_id = 1
+    values = {1: 1, 2: 2}
+
+    select = MagicMock(return_value=values)
+    with patch('geniust.db.Database.select', select):
+        database.user(chat_id, user_data)
+
+    assert user_data == values
+
+
+def test_user_not_in_db(database):
+    user_data = {}
+    chat_id = 1
+
+    select = MagicMock(return_value=None)
+    insert = MagicMock()
+    with patch('geniust.db.Database.select', select), \
+            patch('geniust.db.Database.insert', insert):
+        database.user(chat_id, user_data)
+
+    assert user_data == {'include_annotations': True,
+                         'lyrics_lang': 'English + Non-English',
+                         'bot_lang': 'en',
+                         'token': None
+                         }
+    insert.assert_called_once_with(chat_id,
+                                   True,
+                                   'English + Non-English',
+                                   'en')
+
+
+@patch('psycopg2.connect')
+def test_insert(connection, database):
+    chat_id = 1
+    values = True, 'English', 'en'
+    database.insert(chat_id, *values)
+
+    args = connection().__enter__().cursor().__enter__().execute.call_args
+
+    assert args[0][0] == 'INSERT INTO test_table VALUES (%s, %s, %s, %s);'
+    assert args[0][1] == (chat_id, *values)
+
+
+@patch('psycopg2.connect')
+def test_select(connection, database):
+    chat_id = 1
+    key = 'bot_lang'
+    value = 'en'
+
+    connection().__enter__().cursor().__enter__().fetchall.return_value = [(value,)]
+
+    res = database.select(chat_id, column=key)
+
+    assert res == {key: value}
+
+
+@patch('psycopg2.connect')
+def test_select_all(connection, database):
+    chat_id = 1
+    values = [1, True, 'English', 'en', None]
+
+    return_values = [[x for x in values]]
+    connection().__enter__().cursor().__enter__().fetchall.return_value = return_values
+
+    res = database.select(chat_id)
+
+    assert res['chat_id'] == values[0]
+    assert res['include_annotations'] == values[1]
+    assert res['lyrics_lang'] == values[2]
+    assert res['bot_lang'] == values[3]
+    assert res['token'] == values[4]
+
+
+@patch('psycopg2.connect')
+def test_update(connection, database):
+    chat_id = 1
+    value = 'en'
+    database.update(chat_id, value, 'bot_lang')
+
+    args = connection().__enter__().cursor().__enter__().execute.call_args
+
+    assert args[0][0] == 'UPDATE test_table SET bot_lang = %s WHERE chat_id = 1;'
+    assert args[0][1] == (value,)
+
+
+def test_update_include_annotations(database):
+    chat_id = 1
+    data = False
+
+    update = MagicMock()
+    with patch('geniust.db.Database.update', update):
+        database.update_include_annotations(chat_id, data)
+
+    update.assert_called_once_with(chat_id, data, 'include_annotations')
+
+
+def test_update_lyrics_language(database):
+    chat_id = 1
+    data = 'Non-English'
+
+    update = MagicMock()
+    with patch('geniust.db.Database.update', update):
+        database.update_lyrics_language(chat_id, data)
+
+    update.assert_called_once_with(chat_id, data, 'lyrics_lang')
+
+
+def test_update_bot_language(database):
+    chat_id = 1
+    data = 'fa'
+
+    update = MagicMock()
+    with patch('geniust.db.Database.update', update):
+        database.update_bot_language(chat_id, data)
+
+    update.assert_called_once_with(chat_id, data, 'bot_lang')
+
+
+def test_update_token(database):
+    chat_id = 1
+    data = 'a'
+
+    update = MagicMock()
+    with patch('geniust.db.Database.update', update):
+        database.update_token(chat_id, data)
+
+    update.assert_called_once_with(chat_id, data, 'token')
+
+
+def test_delete_token(database):
+    chat_id = 1
+    data = None
+
+    update = MagicMock()
+    with patch('geniust.db.Database.update', update):
+        database.delete_token(chat_id)
+
+    update.assert_called_once_with(chat_id, data, 'token')
+
+
+def test_get_token(database):
+    chat_id = 1
+    data = 'a'
+
+    select = MagicMock()
+    select.return_value = {'token': data}
+    with patch('geniust.db.Database.select', select):
+        res = database.get_token(chat_id)
+
+    select.assert_called_once_with(chat_id, column='token')
+    assert res == data
+
+
+def test_get_language(database):
+    chat_id = 1
+    data = 'en'
+
+    select = MagicMock()
+    select.return_value = {'bot_lang': data}
+    with patch('geniust.db.Database.select', select):
+        res = database.get_language(chat_id)
+
+    select.assert_called_once_with(chat_id, column='bot_lang')
+    assert res == data
---
+++ tests/test_utils.py
@@ -0,0 +1,203 @@
+import re
+from unittest.mock import patch, MagicMock
+
+import pytest
+from telegram.utils.helpers import create_deep_linked_url
+from bs4 import BeautifulSoup
+
+from geniust import utils, api
+
+
+@pytest.fixture(scope='function')
+def lyrics(page):
+    html = BeautifulSoup(page.replace('<br/>', '\n'), 'html.parser')
+    lyrics = html.find_all("div", class_=re.compile("^lyrics$|Lyrics__Container"))
+    if lyrics[0].get('class')[0] == 'lyrics':
+        lyrics = lyrics[0]
+        lyrics = lyrics.find('p') if lyrics.find('p') else lyrics
+    else:
+        br = html.new_tag('br')
+        try:
+            for div in lyrics[1:]:
+                lyrics[0].append(div).append(br)
+        except Exception as e:
+            msg = f'{str(e)} with {div.attrs}'
+            print(msg)
+        lyrics = lyrics[0]
+
+    api.replace_hrefs(lyrics)
+
+    # remove redundant tags that neither Telegram
+    # nor the other formats (PDF and Telegra.ph) support
+    for tag in lyrics.find_all():
+        if tag.name not in ('br', 'strong​', '​b​', 'em​', '​i​', 'a'):
+            tag.unwrap()
+
+    return str(lyrics).strip()
+
+
+def test_deep_link():
+    username = 'test_bot'
+    entity = {'name': 'test_name',
+              'api_path': 'artist',
+              'id': 1,
+              }
+    url = create_deep_linked_url(username, f"artist_{entity['id']}")
+    final_link = f"""<a href="{url}">{entity['name']}</a>"""
+
+    with patch('geniust.username', username):
+        res = utils.deep_link(entity)
+
+    assert res == final_link
+
+
+def test_remove_unsupported_tags():
+    html = ('<a>t</a>'
+            '<b>t</b>'
+            '<img>'
+            '<u>t</u>'
+            '<invalid>t</invalid>'
+            )
+    soup = BeautifulSoup(html, 'html.parser')
+
+    utils.remove_unsupported_tags(soup)
+
+    assert soup.find('img') is None
+    assert soup.find('invalid') is None
+    assert soup.get_text().count('t') == 4
+
+
+def test_remove_unsupported_tags_my_tags():
+    html = ('<a>t</a>'
+            '<b>t</b>'
+            '<img>'
+            '<u>t</u>'
+            '<invalid>t</invalid>'
+            )
+    soup = BeautifulSoup(html, 'html.parser')
+    supported = ['a', 'b', 'invalid']
+
+    utils.remove_unsupported_tags(soup, supported)
+
+    assert soup.find('img') is None
+    assert soup.find('u') is None
+    assert soup.find('invalid') is not None
+    assert soup.get_text().count('t') == 4
+
+
+def test_remove_extra_newlines():
+    string = "text\n\nsome_text\n\n[verse]\n<br>\nmore_text"
+
+    res = utils.remove_extra_newlines(string)
+
+    assert res.find('\n\n[verse]') != -1
+    assert res.find('text\nsome_text') != -1
+    assert res.find('<br>') == -1
+
+
+def test_remove_links():
+    string = "text\nhttps://example.com\ntext\nhttp://example.com\ntext"
+
+    res = utils.remove_links(string)
+
+    assert res == 'text\ntext\ntext'
+
+
+@pytest.mark.parametrize('language', ['English',
+                                      'Non-English',
+                                      'English + Non-English'])
+def test_format_language_str(language):
+    string = 'line with ütf8 chars.\nline with only ascii chars\nخط فقط با غیر اسمی۲'
+
+    res = utils.format_language(string, language)
+
+    if language == 'English':
+        assert res == '\nline with only ascii chars\n'
+    elif language == 'Non-English':
+        assert res == '\nخط فقط با غیر اسمی۲'
+    else:
+        assert res == string
+
+
+@pytest.mark.parametrize('language', ['English',
+                                      'Non-English',
+                                      'English + Non-English'])
+def test_format_language_soup(lyrics, language):
+    soup = BeautifulSoup(lyrics, 'html.parser')
+
+    utils.format_language(soup, language)
+
+    if language == 'English':
+        lyrics = str(soup.get_text())
+        assert re.match(r'[^\x00-\x7F]', lyrics) is None
+    elif language == 'Non-English':
+        lyrics = re.sub(r'(\[.*?\]|\n)', '', str(soup.get_text()))
+        assert re.match(r'[\x00-\x7F]', lyrics) is None
+    else:
+        assert str(soup) == lyrics
+
+
+@pytest.mark.parametrize('format_type', ['zip',
+                                         'else'])
+def test_format_annotations(lyrics, annotations, format_type):
+
+    num_annotations = len(annotations.keys())
+
+    res = utils.format_annotations(lyrics,
+                                   annotations,
+                                   include_annotations=True,
+                                   format_type=format_type)
+
+    assert len(res.find_all('annotation')) == num_annotations
+
+    if format_type == 'zip':
+        assert res.get_text().count('!--!') == num_annotations
+
+
+@pytest.mark.parametrize('artist, title', [('Genius Translation', 'test_name'),
+                                           ('test_artist', 'test_name')])
+def test_format_title(artist, title):
+
+    res = utils.format_title(artist, title)
+
+    if 'Genius' in artist:
+        assert res == 'test_name'
+    else:
+        assert res == 'test_artist - test_name'
+
+
+@pytest.mark.parametrize('entity', [pytest.lazy_fixture('album'),
+                                    pytest.lazy_fixture('artist'),
+                                    pytest.lazy_fixture('song'),
+                                    {'entity': {}}])
+def test_get_description(entity):
+    entity = entity[list(entity)[0]]
+
+    res = utils.get_description(entity)
+
+    assert isinstance(res, str)
+
+
+@pytest.mark.parametrize('number, result', [(2000, '2000'),
+                                            (10000, '10K'),
+                                            (154000, '154K'),
+                                            (2400000, '2.4M'),
+                                            ])
+def test_human_format(number, result):
+
+    res = utils.human_format(number)
+
+    assert res == result
+
+
+def test_log():
+    func = MagicMock()
+    func.__name__ = 'test_log'
+    logger = MagicMock()
+
+    with patch('logging.getLogger', logger):
+        res = utils.log(func)
+        res(1, 2, a='a')
+
+    func.assert_called_once_with(1, 2, a='a')
+    assert logger().debug.call_count == 2
