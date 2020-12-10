1931ded82095a9060fbe6f9a89f841c3a47cad36
Allerter <45076212+Allerter@users.noreply.github.com>
2020-12-10 18:37:56 +0330

    - fixed tox issues - added dependencies needed for tox - added missing assert in a couple of tests - added generating coverage file in JSON

--- setup.py
+++ setup.py
@@ -10,15 +10,18 @@ with open(here / "runtime.txt", "r") as f:
 
 extras_require = {
     "checks": [
+        "tox==3.20.1",
         "mypy==0.790",
         "black==20.8b1",
         "flake8==3.8.4",
         "flake8-bugbear==20.11.1",
-        "coverage==5.3",
+
     ],
     "tests": [
         "pytest==6.1.2",
+        "pytest-asyncio==0.14.0",
         "pytest-lazy-fixture==0.6.3",
+        "coverage==5.3",
     ],
 }
 
