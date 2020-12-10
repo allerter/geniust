0c8d8e05edb11897bcebf4d511b27e95e1d98ada
Allerter <45076212+Allerter@users.noreply.github.com>
2020-12-10 16:44:08 +0330

    applied black formatting

--- setup.py
+++ setup.py
@@ -4,33 +4,38 @@ from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 
-with open(here / 'requirements.txt', 'r') as f:
+with open(here / "requirements.txt", "r") as f:
     requirements = f.read().split()
 
-with open(here / 'runtime.txt', 'r') as f:
-    runtime = f.read().replace('python-', '')
+with open(here / "runtime.txt", "r") as f:
+    runtime = f.read().replace("python-", "")
 
 
 extras_require = {
-    'checks': [
-        'mypy==0.790',
-        'black==20.8b1',
-        'flake8==3.8.4',
-        'flake8-bugbear==20.11.1',
-        'coverage==5.3'
-
+    "checks": [
+        "mypy==0.790",
+        "black==20.8b1",
+        "flake8==3.8.4",
+        "flake8-bugbear==20.11.1",
+        "coverage==5.3",
     ],
-    'tests': [
-        'pytest==6.1.2',
-        'pytest-lazy-fixture==0.6.3',
-    ]}
+    "tests": [
+        "pytest==6.1.2",
+        "pytest-lazy-fixture==0.6.3",
+    ],
+}
 
-extras_require['dev'] = extras_require['checks'] + extras_require['tests']
+extras_require["dev"] = extras_require["checks"] + extras_require["tests"]
 
 setup(
     name="geniust",
-    packages=find_packages(exclude=('tests', 'tests.*',)),
+    packages=find_packages(
+        exclude=(
+            "tests",
+            "tests.*",
+        )
+    ),
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires='>=' + runtime
+    python_requires=">=" + runtime,
 )
