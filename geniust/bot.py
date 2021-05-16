a08dc9d5f66e64ec6e0d561b2bdf53e4479a8891
allerter <8884518-allerter@users.noreply.gitlab.com>
2021-05-16 10:36:11 +0430

    re-enable error handler

--- geniust/bot.py
+++ geniust/bot.py
@@ -645,7 +645,7 @@ def main():
     dp.add_handler(shuffle_preferences_conv_handler)
 
     # log all errors
-    # dp.add_error_handler(error)
+    dp.add_error_handler(error)
 
     # web hook server to respond to GET cron jobs at /notify
     # and receive user tokens at /callback
