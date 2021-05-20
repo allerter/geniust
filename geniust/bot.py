be375575611c09f661c3566e1d51a35fd1f2aa45
allerter <8884518-allerter@users.noreply.gitlab.com>
2021-05-20 11:20:51 +0430

    Fix supplying wrong handler for add_error_handler

--- geniust/bot.py
+++ geniust/bot.py
@@ -629,7 +629,7 @@ def main():
     dp.add_handler(shuffle_preferences_conv_handler)
 
     # log all errors
-    dp.add_error_handler(error)
+    dp.add_error_handler(error_handler)
 
     # web hook server to respond to GET cron jobs at /notify
     # and receive user tokens at /callback
