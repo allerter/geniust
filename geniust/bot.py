c87a16030ded4d4a022de37a408d8487ccad7924
allerter <8884518-allerter@users.noreply.gitlab.com>
2022-05-05 10:21:27 +0430

    fix returning to main menu from cbq

--- geniust/bot.py
+++ geniust/bot.py
@@ -136,7 +136,7 @@ def main_menu(update: Update, context: CallbackContext) -> int:
     text = context.bot_data["texts"][language]["main_menu"]
 
     message = update.message if update.message else update.callback_query.message
-    is_chat_group = "group" in update.message.chat.type
+    is_chat_group = "group" in message
     reply_to_message_id = message.message_id if is_chat_group else None
 
     logger.debug(
